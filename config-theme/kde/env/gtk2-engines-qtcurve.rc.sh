927aa752e09f6f4af8faed90f00c82e9f4b82ea6
falkTX <falktx@gmail.com>
2012-12-29 20:01:10 +0000

    Theme fixes

--- config-theme/kde/env/gtk2-engines-qtcurve.rc.sh
+++ config-theme/kde/env/gtk2-engines-qtcurve.rc.sh
@@ -2,6 +2,6 @@
 
 if [ -f $HOME/.gtkrc-2.0-kde4 ]; then
   export GTK2_RC_FILES=$HOME/.gtkrc-2.0-kde4
-else
+elif [ -f $HOME/.gtkrc-2.0-kxstudio ]; then
   export GTK2_RC_FILES=$HOME/.gtkrc-2.0-kxstudio
 fi
